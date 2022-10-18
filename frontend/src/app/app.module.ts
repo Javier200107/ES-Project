7f75007fe6b91e006cbd0567fcf916b98c44824b
rubengimbertroldan <gimbertruben@gmail.com>
2022-10-18 23:26:20 +0200

    Comprovació camps del register

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,8 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from "@angular/material/input";
 import {MatFormFieldModule} from '@angular/material/form-field';
 
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
 import {MatRadioModule} from "@angular/material/radio";
 import {MatButtonModule} from '@angular/material/button';
 
