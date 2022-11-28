085433a50894641610950c405eb3055cb24e8805
Oscar Lacasta <olacasle7@alumnes.ub.edu>
2022-11-28 17:58:27 +0100

    improved config.py, allow loading secrets from the env

--- backend/config.py
+++ backend/config.py
@@ -1,28 +1,30 @@
-# Flask configuration
+from decouple import config as config_decouple
 
-class Config:
-    pass
+_AzureDB = "postgresql://Javier@enginyeriadelsoftware-server:" \
+           "EnginyeriaDelSoftware2022@enginyeriadelsoftware-server.postgres.database.azure.com:5432/postgres"
+
+_PRODUCTION = config_decouple("PRODUCTION", cast=bool, default=False)
+
+
+class Config(object):
+    PRODUCTION = _PRODUCTION
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = config_decouple("SECRET_KEY", default="p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B")
+    STATIC_URL_PATH = ""
+    STATIC_FOLDER = "/static"
+    TEMPLATE_FOLDER = "/templates"
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://Javier@enginyeriadelsoftware-server:EnginyeriaDelSoftware2022" \
-                              "@enginyeriadelsoftware-server.postgres.database.azure.com:5432/postgres "
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_URL_PATH = ""
-    STATIC_FOLDER = "/static"
-    TEMPLATE_FOLDER = "/templates"
-    SECRET_KEY = "p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B"
+    SQLALCHEMY_DATABASE_URI = config_decouple("SQLALCHEMY_DATABASE_URI", default=_AzureDB)
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B"
-    STATIC_URL_PATH = ""
-    STATIC_FOLDER = "/static"
-    TEMPLATE_FOLDER = "/templates"
+    SQLALCHEMY_DATABASE_URI = config_decouple("SQLALCHEMY_DATABASE_URI", default="sqlite:///data.db")
 
 
-configuration = {"development": DevelopmentConfig, "production": ProductionConfig}
+configurations = {"development": DevelopmentConfig, "production": ProductionConfig}
+
+environment = configurations["development"] if not _PRODUCTION else configurations["production"]
