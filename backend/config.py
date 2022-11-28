54baf698a2c63616d8855c74efdfe0d573c545c3
Oscar Lacasta <olacasle7@alumnes.ub.edu>
2022-11-28 17:47:40 +0100

    temp revert

--- backend/config.py
+++ backend/config.py
@@ -1,30 +1,28 @@
-from decouple import config as config_decouple
-
-_AzureDB = "postgresql://Javier@enginyeriadelsoftware-server:" \
-           "EnginyeriaDelSoftware2022@enginyeriadelsoftware-server.postgres.database.azure.com:5432/postgres"
-
-_PRODUCTION = config_decouple('PRODUCTION', cast=bool, default=False)
-
+# Flask configuration
 
 class Config:
-    PRODUCTION = _PRODUCTION
+    pass
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://Javier@enginyeriadelsoftware-server:EnginyeriaDelSoftware2022" \
+                              "@enginyeriadelsoftware-server.postgres.database.azure.com:5432/postgres "
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = config_decouple('SECRET_KEY', default="p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B")
+    STATIC_URL_PATH = ""
+    STATIC_FOLDER = "/static"
+    TEMPLATE_FOLDER = "/templates"
+    SECRET_KEY = "p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "p2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B"
     STATIC_URL_PATH = ""
     STATIC_FOLDER = "/static"
     TEMPLATE_FOLDER = "/templates"
 
 
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = config_decouple('SQLALCHEMY_DATABASE_URI', default=_AzureDB)
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = config_decouple('SQLALCHEMY_DATABASE_URI', default="sqlite:///data.db")
-
-
-configurations = {"development": DevelopmentConfig, "production": ProductionConfig}
-
-environment = configurations['development'] if not _PRODUCTION else configurations['production']
+configuration = {"development": DevelopmentConfig, "production": ProductionConfig}
