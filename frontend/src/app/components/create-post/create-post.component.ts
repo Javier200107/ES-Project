6151b7f1a3e99255cb1d78c34e182cb015982c7c
AdriaValls <avallsmo12@alumnes.ub.edu>
2022-11-30 11:44:54 +0100

    Ara s'actualitza el main quan es fa el post

--- frontend/src/app/components/create-post/create-post.component.ts
+++ frontend/src/app/components/create-post/create-post.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { PostCreationService } from "../../services/post-creation.service";
 import { NewPostForm } from "../../models/NewPostForm";
@@ -12,6 +12,8 @@ import {Post} from "../../models/Post";
 export class CreatePostComponent implements OnInit {
 
   @Input() token!: string;
+  @Output() newPostEvent = new EventEmitter<Post>();
+
   public postForm!: FormGroup;
   post_content!: string;
 
@@ -34,7 +36,12 @@ export class CreatePostComponent implements OnInit {
 
     }
     this.postCreator.createPost(postContent, this.token).subscribe((newPost: Post) =>{
-      this.newPost = newPost;
+
+      // @ts-ignore
+      console.log(newPost['post'])
+      // @ts-ignore
+      this.newPostEvent.emit(newPost['post'])
+
     }, (error: any) => {
       console.log(error);
     })
