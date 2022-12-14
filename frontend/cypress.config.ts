a77bd6a9435ce4afda78de7e3798681bf8d0ba5a
rubengimbertroldan <gimbertruben@gmail.com>
2022-12-14 11:25:49 +0100

    Fixed edit profile + Static FE

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 
   e2e: {
-    'baseUrl': 'https://enginyeriadelsoftware-f3.azurewebsites.net',
+    'baseUrl': 'http://localhost:4200',
     supportFile: false
   },
 
