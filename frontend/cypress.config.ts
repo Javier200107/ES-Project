6915c3661c8fdcf0efb98200cff589b5ef7faf8c
rubengimbertroldan <gimbertruben@gmail.com>
2022-12-14 09:06:22 +0100

    Github action e2e

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 
   e2e: {
-    'baseUrl': 'http://localhost:4200',
+    'baseUrl': 'https://enginyeriadelsoftware-f3.azurewebsites.net',
     supportFile: false
   },
 
