e6a6c9feae35fe7845c0476afe1a036d1d4b3e72
Oscar Lacasta <olacasle7@alumnes.ub.edu>
2022-12-16 10:04:38 +0100

    fix import line format

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from backend.config import environment
 from backend.db import db
-from backend.resources.accounts import (
-    Accounts,
-    AccountsFiles,
-    AccountsFilesStatic,
-    AccountsList,
-)
+from backend.resources.accounts import Accounts, AccountsFiles, AccountsFilesStatic, AccountsList
 from backend.resources.follow import Follow, ListFollowing, ListFollows, PostsFollowing
 from backend.resources.like import Like, ListPostLikes, ListUserLikes
 from backend.resources.login import Login
